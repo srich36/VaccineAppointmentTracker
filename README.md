68b585393399cc64b8169080f78236b21bb2438a
Sean Rich <srich366@gmail.com>
2021-04-10 11:27:34 -0700

    [Vaccine Tracker] Add output stream

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ pip3 install -r requirements.txt
 ```
 2. Set configuration parameters (current location, mile threshold, desired vaccine types, desired sort order)
 3. Run the script 	python3 main.py`
+
+## TODO:
+
+- Add email notifications
--- main.py
+++ main.py
@@ -8,6 +8,31 @@ from datetime import datetime
 
 url = 'https://www.vaccinespotter.org/api/v0/states/CA.json'
 
+output_str = ''
+
+
+class CustomOutput:
+    """
+    Capture everything we send to stdout into a variable so we can send it in an email notification too
+    """
+
+    def __init__(self, stream):
+
+        self.stream = stream
+
+    def write(self, data):
+        global output_str
+        self.stream.write(data)
+        self.stream.flush()
+        output_str += data
+
+    def flush(self):
+        pass
+
+
+sys.stdout = CustomOutput(sys.stdout)
+
+
 class VaccineTypes:
     UNKNOWN = 'unknown'
     PFIZER = 'pfizer'
@@ -21,19 +46,22 @@ class SortOrder:
     APPOINTMENTS_LAST_FETCHED = 'last_fetched_ago'
     APPOINTMENTS_LAST_MODIFIED = 'last_modified_ago'
 
+
 #### CONFIGURATION ####
 current_lat = 37.40758515947319
 current_long = -121.9368054864926
 current_location = (current_lat, current_long)
 
 # Distance Threshold in miles
-threshold = 25
+threshold = 50
 
-sort_order = SortOrder.APPOINTMENTS_LAST_MODIFIED
-desired_types = set([VaccineTypes.UNKNOWN, VaccineTypes.PFIZER, VaccineTypes.MODERNA, VaccineTypes.ALL, VaccineTypes.JJ])
+sort_orders = set([SortOrder.APPOINTMENTS_LAST_MODIFIED, SortOrder.LOCATION])
+desired_types = set([VaccineTypes.UNKNOWN, VaccineTypes.PFIZER,
+                     VaccineTypes.MODERNA, VaccineTypes.ALL, VaccineTypes.JJ])
 #### CONFIGURATION ####
 
 current_time = datetime.utcnow().replace(tzinfo=pytz.utc)
+
 
 def get_distance_away(site):
     coords = site['geometry']['coordinates']
@@ -64,35 +92,49 @@ def output_date_delta(td_object):
         ('second',      1)
     ]
 
-    strings=[]
+    strings = []
     for period_name, period_seconds in periods:
         if seconds > period_seconds:
-            period_value , seconds = divmod(seconds, period_seconds)
+            period_value, seconds = divmod(seconds, period_seconds)
             has_s = 's' if period_value > 1 else ''
             strings.append("%s %s%s" % (period_value, period_name, has_s))
 
     return ", ".join(strings)
 
 
+separator = '-' * 80
+
+
+def send_email_notification(sites):
+    # TODO: Implement
+    # Get site IDs (site["properties"]["id"])
+    # Check lockfile (and lockfile created date for each site)
+    # Load to emails from config dynamically here 
+    # If any sites don't have lockfiles, send content in output_str in email
+    pass
+
+
 def output_site(site, index):
-    separator = '-' * 80
     appointments = site["properties"]["appointments"]
     appointment_types = set()
     for appointment in appointments:
-        for appointment_type in appointment['appointment_types']:
+        for appointment_type in appointment.get('appointment_types', []):
             appointment_types.add(appointment_type)
 
     print(f'{index}.')
     print(f'Distance: {site["distance_away"]}')
     print(f'URL: {site["properties"]["url"]}')
-    print(f'Place: {site["properties"]["name"]} - {site["properties"]["address"]}')
+    print(
+        f'Place: {site["properties"]["name"]} - {site["properties"]["address"]}')
     print(f'Zip code: {site["properties"]["postal_code"]}')
     print(f'Number of appointments: {len(site["properties"]["appointments"])}')
-    print(f'Vaccine types: {",".join(site["properties"]["appointment_vaccine_types"])}')
+    print(
+        f'Vaccine types: {",".join(site["properties"]["appointment_vaccine_types"])}')
     print(f'Appointment types: {",".join(appointment_types)}')
     print(f'Last modified ago: {output_date_delta(site["last_modified_ago"])}')
     print(f'Last fetched ago: {output_date_delta(site["last_fetched_ago"])}')
     print(separator)
+
 
 res = requests.get(url)
 if not 200 >= res.status_code <= 300:
@@ -101,26 +143,37 @@ if not 200 >= res.status_code <= 300:
 
 vaccines_checked = res.json()['features']
 availble_vaccines = [location for location in vaccines_checked
-                     if location['properties']['appointments_available'] is True and 
+                     if location['properties']['appointments_available'] is True and
                      len(location['properties']['appointments']) > 0
                      and has_desired_vaccine_type(location['properties']['appointment_vaccine_types'])
                      ]
 
-close_sites = [ site for site in availble_vaccines if under_distance_threshold(site) ]
+close_sites = [
+    site for site in availble_vaccines if under_distance_threshold(site)]
 for close_site in close_sites:
     close_site['distance_away'] = get_distance_away(close_site)
-    appointments_last_modified = parser.parse(close_site["properties"]["appointments_last_modified"])
-    appointments_last_fetched = parser.parse(close_site["properties"]["appointments_last_fetched"])
+    appointments_last_modified = parser.parse(
+        close_site["properties"]["appointments_last_modified"])
+    appointments_last_fetched = parser.parse(
+        close_site["properties"]["appointments_last_fetched"])
     last_modified_ago = current_time - appointments_last_modified
     last_fetched_ago = current_time - appointments_last_fetched
     close_site['last_modified_ago'] = last_modified_ago
     close_site['last_fetched_ago'] = last_fetched_ago
 
-close_sites.sort(key=lambda site: site[sort_order])
+
+num_sites = len(close_sites)
+print(f'{num_sites} sites found')
+
+for sort_order in sort_orders:
+    print()
+    print(separator)
+    print(f'Sorting by: {sort_order}')
+    close_sites.sort(key=lambda site: site[sort_order])
+    for idx, result in enumerate(close_sites):
+        output_site(result, idx + 1)
 
 
-print(f'{len(close_sites)} sites found')
-print('-' * 80)
+if num_sites > 0:
+    send_email_notification(close_sites)
 
-for idx, result in enumerate(close_sites):
-    output_site(result, idx + 1)
